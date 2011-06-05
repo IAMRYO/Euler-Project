a28f8a16bc32d9e9dce129c820d8f9e7034b715c
Ryan Clark <rclark.sf@gmail.com>
2011-06-05 21:56:06 +0000

    correct answer to problem1

--- problem1.rb
+++ problem1.rb
@@ -1,3 +1,11 @@
-c = 100
+c = 0
+d = 0
+e = 0
 
-puts c
+c.step(999, 3) { |number| c += number }
+
+d.step(999, 5) { |number| d += number }
+
+e.step(999, 15) { |number| e += number }
+
+puts (c + d - e)
